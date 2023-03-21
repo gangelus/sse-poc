5e5d944a4cf3b33a40dc86e06c6b387457260a22
Gabriel Angelus <gabriel@condominiodedicado.com.br>
2023-03-21 17:25:59 -0300

    less verbosity

--- src/index.js
+++ src/index.js
@@ -1,53 +1,32 @@
-const { randomUUID } = require('crypto')
-const startServer = require('./server.js')
-
-const app = startServer()
+const app = require('./server.js')()
 
 // Lista de clientes conectados
-const clients = [];
+const connections = [];
 
 app.get('/events', (req, res) => {
-    const uuid = randomUUID();
-
     // Enviar cabeçalhos na resposta
     res.writeHead(200, {
       'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
       'Connection': 'keep-alive'
     });
 
-    // Enviar mensagem para cliente
-    res.write('data: connected\n\n');
+    // Adicionar conexão na lista
+    connections.push(res);
 
-    // Adicionar cliente na lista
-    clients.push({
-        uuid,
-        res
-    })
-
-    // Remover cliente da lista após evento de desconexão
-    res.on('close', () => {
-        const indexToRemove = clients.findIndex(c => c.uuid == uuid)
-        clients.splice(indexToRemove, 1);
-    });
+    res.write('\n\n');
 });
 
-app.get('/emit/:eventName', (req, res) => {
-    const { eventName } = req.params;
-
+app.get('/emit', (req, res) => {
     // Para cada cliente conectado no momento
-    for (const client of clients) {
-        // Hora de agora
-        const now = new Date().toLocaleTimeString();
+    for (const conn of connections) {
+        // Payload
+        const data = JSON.stringify({ name: 'Gabriel' });
 
-        // Enviar mensagem com payload
-        client.res.write(
-            `event: ${eventName}\n` +
-            `data: ${now}\n\n`
+        conn.write(
+            `event: current-time\n` +
+            `data: ${data}\n\n`
         );
     }
 
-    res.json({
-        message: `Event ${eventName} emitted to ${clients.length} clients.`
-    });
+    res.status(200).end();
 })
